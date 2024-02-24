d34a408b32aa588c1cfb35341a72c1fb9caec2e7
ASviat <ecrprague.asi@gmail.com>
2024-02-24 19:19:34 +0300

    Person

---
+++ Person/src/Person.java
@@ -0,0 +1,50 @@
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+
+        this.age = age < 0 || age > 100 ? 0 : age;
+    }
+
+    public boolean isTeen() {
+        return age > 12 && age < 20;
+    }
+
+    public String getFullName() {
+        return firstName.isEmpty() && lastName.isEmpty() ? ""
+                : lastName.isEmpty() ? firstName
+                : firstName.isEmpty() ? lastName : firstName + " " + lastName;
+    }
+
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.firstName = "John";
+        person.lastName = "Smith";
+
+        System.out.println(person.getFullName());
+
+    }
+}
